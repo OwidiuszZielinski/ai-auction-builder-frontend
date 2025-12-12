d7ab54333981b363f2fe590b334dee78388809ec
root <root@marek234.mikr.us>
2025-12-12 11:11:36 +0100

    Added List

--- src/App.vue
+++ src/App.vue
@@ -9,5 +9,67 @@
   </p>
   <h1>Test</h1>
 </template>
+- [ ] Muszynianka 6 x 1.5l
+- [ ] Muszynianka 6 x 1l
+- [ ] Żywiec Zdrój 6 x 1.5l
+- [ ] Kurczak 2 op.
+- [ ] Mieso mielone 2 op.
+- [ ] Łosoś puszka 4 op.
+- [ ] Łosoś filet 1 op.
+- [ ] Passata 2 szt.
+- [ ] Mleko 6 x 1l.
+- [ ] Majonez 400ml
+- [ ] Tortilla wieloziarnista 2 op.
+- [ ] Ryż 4 x 100g
+- [ ] Mango
+- [ ] ogórek 2 szt.
+- [ ] Pomidory małe 2 op.
+- [ ] Pomidory malinowe 4 szt.
+- [ ] Serek almette 2 op.
+- [ ] Mozzarella kostka
+- [ ] Ser żólty 1 op.
+- [ ] Parówki 1 op.
+- [ ] Chleby 5 szt.
+- [ ] Dżem 1 op.
+- [ ] Pizza mrożona 2 op.
+- [ ] Nuggetsy 1 op.
+- [ ] Ziemniaczki (Kaufland)
+- [ ] Wędlina 3 rodzaje pakowanej
+- [ ] Serek wiejski z owocami 6 op.
+- [ ] Skyr 2 szt.
+- [ ] Jogurt grecki 400ml
+- [ ] Cebula
+- [ ] Awokado
+- [ ] Pomarańcze
+- [ ] Cytryna
+- [ ] Imbir
+- [ ] Klinek 2 op.
+- [ ] Pastella
+- [ ] Roszponka/szpinak
+- [ ] Małe mozzarelle
 
+
+
+- [ ] Ciasto francuzkie 3 op
+- [ ] Kabanosy 2 op.
+- [ ] Mozzarella kostka
+- [ ] Ketchup 
+- [ ] Tortilla 2 op.
+- [ ] Almette 2 op.
+- [ ] Losos kanapkowy 1 op.
+- [ ] Ogorek 1 szt.
+- [ ] Kuskus
+- [ ] Mozzarela mala
+- [ ] Mango
+- [ ] Pomidorki
+- [ ] Kurczak
+- [ ] Roszponka
+- [ ] Chrupki 4 op
+- [ ] Cola 3 szt
+- [ ] Sok
+- [ ] Wino dla zbiety
+* * *
+* * *
+* * *
+* * *
 <style scoped></style>
